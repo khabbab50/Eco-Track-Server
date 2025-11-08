f2e74fe20a6210f539fd3f17285899632ce659a2
Khabbab Hossen <sk.khabbab50@gmail.com>
2025-11-09 00:14:02 +0600

    ecoTrack server site successfuly connect the mobgodb database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +8,36 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+//
+const uri =
+  "mongodb+srv://ecoTrack:PWXCgOydasBvIHpy@cluster0.q4wcqfi.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("EcoTrack Server is running");
 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`EcoTrack Server is running on port ${port}`);
